b6bc911a0912596403980cb72680bde689796f96
Kristen Tidmuss <11579255+KristenTidmuss@users.noreply.github.com>
2021-03-30 21:14:37 +0100

    First load of buttons

---
+++ Server/Controllers/APIController.cs
@@ -0,0 +1,28 @@
+﻿using ElectronicFireRegister.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectronicFireRegister.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class APIController : ControllerBase
+    {
+        private readonly ILogger<APIController> _logger;
+
+        public APIController(ILogger<APIController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<string> Get(string Initials)
+        {
+            return null;
+        }
+    }
+}
